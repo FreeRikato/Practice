81b12c49e9cb5f56cc39402bd9f25f1cba81453b
Rikato <aravinthan@student.tce.edu>
2023-10-09 18:51:26 +0530

    dave gray chapter - 4

---
+++ dave_gray_nodejs/C4 - Event emitter/index.js
@@ -0,0 +1,16 @@
+const logEvents = require("./logEvent");
+
+const EventEmitter = require("events");
+
+class MyEmitter extends EventEmitter {}
+
+// initialize object
+const myEmitter = new MyEmitter();
+
+// add listener for the log event
+myEmitter.on("log", (msg) => logEvents(msg));
+
+setTimeout(() => {
+  //Emit event
+  myEmitter.emit("log", "Log event emitted!");
+}, 2000);
